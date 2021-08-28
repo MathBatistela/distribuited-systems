5ffae7c8fac05aced48ebb0c6f5c136c40f45f93
Matheus Batistela <matheushenriquebatistela@gmail.com>
2021-08-27 21:55:20 -0300

    refacts rest client

--- client/client-REST.rb
+++ client/client-REST.rb
@@ -1,25 +1,35 @@
 require 'json'
 require 'uri'
 require 'net/http'
+require 'http'
 
 
-
-def send_info(body,uri_op,type_op)
+def get_info(body,uri_op,type_op)
     port = 5000.to_s
-    uri = URI('http://127.0.0.1:'+port.to_s+uri_op)
+    url = 'http://127.0.0.1:'+port.to_s+uri_op
+    
     if type_op == "GET"
-        http = Net::HTTP.new(uri.host, uri.port)
-        req = Net::HTTP::Get.new(uri.path, 'Content-Type' => 'application/json')
-        req.body = body
-        res = http.request(req)
-        puts "response #{res.body}"
+        response = HTTP.get(url)
+        for index in 0 ... response.parse.size
+            student = response.parse[index]
+            puts "---------------------------"
+            print "\nStudent #{index}\n\n"
+            puts "RA: #{student["ra"]}"
+            puts "name: #{student["name"]}"
+            puts "period: #{student["period"]}"
+            puts "course code: #{student["course_code"]}"
+            puts "---------------------------"
+        end
+        
     elsif type_op == "POST"
+        uri = URI(url)
         http = Net::HTTP.new(uri.host, uri.port)
         req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
         req.body = body
         res = http.request(req)
         puts "response #{res.body}"
     elsif type_op == "DELETE"
+        uri = URI(url)
         http = Net::HTTP.new(uri.host, uri.port)
         req = Net::HTTP::Delete.new(uri.path, 'Content-Type' => 'application/json')
         req.body = body
@@ -31,76 +41,71 @@ rescue => e
     puts "failed #{e}"
 end
 
-# puts "Informe a porta para conexao"
-# student_ra = gets.chomp.to_i
-
-
 loopFlag = true
 while loopFlag
-    puts "1 - Adicionar Nota do Aluno:"
-    puts "2 - Remover Nota do Aluno:"
-    puts "3 - Consultar Aluno"
-    puts "0 para sair:"
+    puts "1 - Add student grade:"
+    puts "2 - Remove student grade:"
+    puts "3 - Search students by subject"
+    puts "0 - To exit:"
     choose = gets.chomp.to_i
     case choose
     when 1
-        puts "Informe Subject Code"
+        puts "Subject Code:"
         subject_code = gets.chomp
-        puts "Informe o RA do aluno"
+        puts "RA:"
         student_ra = gets.chomp.to_i
-        puts "Informe o Ano"
+        puts "Year:"
         year = gets.chomp.to_i
-        puts "Informe o Semestre"
+        puts "Semester:"
         semester = gets.chomp.to_i
-        # puts "Informe o Grade"
-        # grade = gets.chomp.to_f
-        # puts "Informe o numero de faltas"
-        # abscenses = gets.chomp.to_i
-        body = {
-            'subject_code':subject_code,
-            'student_ra':student_ra,
-            'year':year,
-            'semester':semester,
-        }.to_json
-        # 'grade':grade,
-        # 'abscenses':abscenses,
-        send_info(body,'/enrollment','PUT')
-    when 2
-        puts "Informe Subject Code"
-        subject_code = gets.chomp
-        puts "Informe o RA do aluno"
-        student_ra = gets.chomp.to_i
-        puts "Informe o Ano"
-        year = gets.chomp.to_i
-        puts "Informe o Semestre"
-        semester = gets.chomp.to_i
-        puts "Informe o Grade"
+        puts "Grade:"
         grade = gets.chomp.to_f
+        puts "Abscenses:"
+        abscenses = gets.chomp.to_i
+
         body = {
             'subject_code':subject_code,
             'student_ra':student_ra,
             'year':year,
             'semester':semester,
-            'grade':grade
+            'grade':grade,
+            'abscenses':abscenses
         }.to_json
-        send_info(body,'/enrollment','DELETE')
+
+        get_info(body,'/enrollment','POST')
+    when 2
+        puts "Subject Code:"
+        subject_code = gets.chomp
+        puts "RA:"
+        student_ra = gets.chomp.to_i
+        puts "Year:"
+        year = gets.chomp.to_i
+        puts "Semester:"
+        semester = gets.chomp.to_i
+        body = {
+            'subject_code':subject_code,
+            'student_ra':student_ra,
+            'year':year,
+            'semester':semester,
+        }.to_json
+        get_info(body,'/enrollment','DELETE')
     when 3
-        puts "Informe o codigo da materia"
+        puts "Subject code:"
         subject_code = gets.chomp
         
-        puts "Opcional: Informe o ano para pesquisar"
-        year = gets.chomp.to_i
+        puts "Year (Optional):"
+        year = gets.chomp
         
-        puts "Opcional:  Informe o semestre"
-        semester = gets.chomp.to_i
-        body = {'subject_code':subject_code}
-        if year != ""
-            body['year'] = year
-        elsif semester != ""
-            body['subject_code'] = subject_code
-        end
-        
-        send_info(body.to_json,"students/enrolled?subject_code="+subject_code,"GETURL")
+        puts "Semester (Optional):"
+        semester = gets.chomp
+
+        get_info( 
+            nil,
+            "/students/enrolled?subject_code=" + subject_code + 
+            (year.empty? ? "" : "&year=#{year}") + 
+            (semester.empty? ? "" : "&semester=#{semester}" ),
+            "GET"
+        )
     when 0
         puts "Saindo ...."
         loopFlag = false
